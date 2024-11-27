629a7acda36c4a88488f144736dd17d32d46ebaa
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-11-27 20:18:21 -0300

    Squashed commit of the following:
    commit b0585ab7b766a234aebdf5988133232858cbcb72
    Author: MuriloFiFer <murilo.fischer.senai23@outlook.com>
    Date:   Wed Nov 27 20:17:20 2024 -0300
    
        11
    
    commit ad46962290409694955133e692d318338f45a0c2
    Merge: ca2dedb c00c80e
    Author: MuriloFiFer <murilo.fischer.senai23@outlook.com>
    Date:   Wed Nov 27 20:03:33 2024 -0300
    
        Merge branch 'main' of https://github.com/kelvenbrito/Projeto_Final_Innovatec
    
    commit ca2dedbfbf2b0221bdf39feeb815a6324466f374
    Author: MuriloFiFer <murilo.fischer.senai23@outlook.com>
    Date:   Wed Nov 27 19:56:03 2024 -0300
    
        27/11/24 index,rotas para login.html
    
        alterado nome do arquivo cadastar-usuarios.html para login.html
    
    commit c00c80ed708ce1a175f5078aa112172b8faa5446
    Author: GuilhermeVM2 <123783825+GuilhermeVM2@users.noreply.github.com>
    Date:   Wed Nov 27 19:48:52 2024 -0300
    
        Add files via upload
    
    commit c969927115cf8b881e9a21e4b0f459d74f971fc4
    Merge: 4ed5e83 5dd4058
    Author: MuriloFiFer <murilo.fischer.senai23@outlook.com>
    Date:   Wed Nov 27 19:23:41 2024 -0300
    
        27/11
    
    commit 4ed5e83f53846d5c8bbb8026cc861572cf1b19d3
    Author: MuriloFiFer <murilo.fischer.senai23@outlook.com>
    Date:   Wed Nov 27 19:15:43 2024 -0300
    
        27/11
    
    commit 5dd4058aa0ddc2918c406c9731c5a5edfabe856d
    Author: kelvenbrito <kelvenbrito14@gmail.com>
    Date:   Tue Nov 26 20:10:57 2024 -0300
    
        26/11/24
    
    commit 6ebdeb95cec7478aadcb480c7ec6b730edd5b3a8
    Author: kelvenbrito <kelvenbrito14@gmail.com>
    Date:   Tue Nov 26 20:07:41 2024 -0300
    
        26/11/24
    
    commit 68f6b50ff88fc441173f03e89d6b4611caa3d80e
    Author: MuriloFiFer <murilo.fischer.senai23@outlook.com>
    Date:   Tue Nov 26 20:01:43 2024 -0300
    
          Changes to be committed:
            new file:   .vscode/settings.json
        	modified:   java-spring/src/main/resources/templates/index.html
    
    commit 2f9001fd768ae905989c510fc254d0af90a17309
    Merge: c7842ce b619cd0
    Author: MuriloFiFer <murilo.fischer.senai23@outlook.com>
    Date:   Tue Nov 26 19:57:24 2024 -0300
    
        Merge branch 'main' of https://github.com/kelvenbrito/Projeto_Final_Innovatec
    
    commit c7842ce7bfa4d8f2644fd51fa399427aaf521a7f
    Author: MuriloFiFer <murilo.fischer.senai23@outlook.com>
    Date:   Tue Nov 26 19:50:13 2024 -0300
    
        26/11/2024

---
+++ java-spring/src/main/java/com/example/industria/models/Machine.java
@@ -0,0 +1,54 @@
+package com.example.industria.models;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Machine {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<SparePart> getSpareParts() {
+        return spareParts;
+    }
+
+    public void setSpareParts(List<SparePart> spareParts) {
+        this.spareParts = spareParts;
+    }
+
+    @Id
+    private String id;
+    private String name;
+    private String description;
+
+    @OneToMany(mappedBy = "machine", cascade = CascadeType.ALL)
+    private List<SparePart> spareParts;
+
+    // Getters e Setters
+}
+
---
+++ java-spring/src/main/java/com/example/industria/repositories/UserRepository.java
@@ -0,0 +1,5 @@
+package com.example.industria.repositories;
+
+public class UserRepository {
+    
+}
